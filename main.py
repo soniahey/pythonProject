986adafa47d273ad304a1f18c748f890c00eb125
soniahey <79365004+soniahey@users.noreply.github.com>
2021-02-21 21:39:17 +0900

    specified namespace of api

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_restplus import Resource, Api
 
+# Flask App initialization
 app = Flask(__name__)
+
+api = Api(app, version='1.0', title='API title',
+          description='API description',
+          )
+
+nspace = api.namespace('custom', description='API namespace')
+
+# SWAGGER_UI_DOC_EXPANSION
+app.config.SWAGGER_UI_DOC_EXPANSION = 'full'
 
 @app.route("/")
 def hello():
@@ -13,3 +24,4 @@ def print_hi(name):
 
 if __name__ == '__main__':
     print_hi('Python!')
+
