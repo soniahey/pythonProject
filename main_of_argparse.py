a45272f203328d06667df8208e37a567ee00a4f7
soniahey <79365004+soniahey@users.noreply.github.com>
2021-02-24 14:21:35 +0900

    custom argparse code

--- main_of_argparse.py
+++ main_of_argparse.py
@@ -1,12 +1,16 @@
 # https://docs.python.org/ko/3.5/library/argparse.html 참고
 import argparse
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
+parser = argparse.ArgumentParser(description='정수 관련 계산')
+
+# positional arguments:
+parser.add_argument('ints', metavar='N', type=int, nargs='+',
+                    help='계산을 위한 정수')
+# optional arguments:
 parser.add_argument('--sum', dest='accumulate', action='store_const',
                     const=sum, default=max,
-                    help='sum the integers (default: find the max)')
+                    help='정수들의 합 (default: 가장 큰 수 찾기)')
 
 args = parser.parse_args()
-print(args.accumulate(args.integers))
+print("결과: ", args.accumulate(args.ints))
+
